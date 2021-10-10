8daa4c92409b92dd7dea449c5d73ce6e5ba884f3
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-10 21:05:28 +0530

    Single Playes at the position 0

---
+++ Snake and Ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
